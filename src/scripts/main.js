6d787257577432677af84bcc03406fe55d7dff33
Stanislav <stasusss07@gmail.com>
2020-06-18 01:27:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello mate academy!');
+const galleryList = document.querySelector('.gallery__list');
+const galleryLargeImage = document.querySelector('.gallery__large-img');
+
+galleryList.addEventListener('click', changeLargeImage);
+
+function changeLargeImage(action) {
+  action.preventDefault();
+
+  const link = action.target.closest('a');
+
+  galleryLargeImage.setAttribute('src', link.href);
+}
